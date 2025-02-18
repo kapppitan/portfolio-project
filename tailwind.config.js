8845b0388a52c700e3018b69313f651f19a14060
kapppitan <neezamme1403@gmail.com>
2025-02-18 10:57:23 +0800

    Created a simple hero page.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                customFont: ['Modulus']
+            },
+            cursor: {
+                default: 'url("cursor.png"), default',
+                pointer: 'url("clicker.png"), pointer',
+            }
+        },
+    },
+    plugins: [],
+}
