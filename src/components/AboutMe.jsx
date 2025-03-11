3554b00717f85f624a5b4a022cdbf7e197c5d580
kapppitan <neezamme1403@gmail.com>
2025-03-11 17:40:33 +0800

    Made the interface responsive.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,67 +1,87 @@
-import { useEffect } from "react"
-import Aos from "aos"
+import { useEffect, useState } from "react";
+import Aos from "aos";
 
 export function AboutMe() {
   useEffect(() => {
-      Aos.init({})
-  }, [])
+    Aos.init({});
+  }, []);
+
+  const [index, setIndex] = useState(0);
+
+  const handleNext = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % 3);
+  };
+
+  const handlePrev = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + 3) % 3);
+  };
 
   return (
     <div className='flex h-content items-center justify-center' id="about-me">
-      <div className='flex flex-col justify-center items-center gap-25 py-40' data-aos='zoom-in-down'>
-        <div className='w-6xl h-content bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d] overflow-hidden shadow-lg'>
+      <div className='flex flex-col justify-center items-center gap-25 py-10 lg:py-40' data-aos='zoom-in-down'>
+        {/* About Me */}
+        <div className='w-70 lg:w-6xl h-content bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d] overflow-hidden shadow-lg'>
           <div className='p-2.5 bg-[#c3e7e5] flex justify-between items-center'>
-            <p className='flex-grow text-center text-3xl mt-2'>About Me</p>
+            <p className='flex-grow text-center text-1xl lg:text-3xl'>About Me</p>
             <a className='w-8 rounded-[100%] bg-red-400 flex justify-center items-center border-4' href="#hero">‎</a>
           </div>
 
           <hr className='border-b-4' />
 
-          <div className='flex flex-col p-15 gap-25 items-center'>
-            <div className="flex gap-8 items-center">
+          <div className='flex flex-col p-5 lg:p-15 gap-25 items-center'>
+            <div className="flex flex-col lg:flex-row gap-8 items-center">
+              <img src="capybara.jpg" alt="image" className="h-50 flex lg:hidden bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d]" />
+
               <div className="w-full">
-                <p className="text-3xl">My name is</p>
-                <h1 className="text-5xl">Raphydhar-Neezamme J. Ibrahim</h1>
+                <p className="text-sm lg:text-3xl">My name is</p>
+                <h1 className="text-1xl lg:text-5xl font-bold">Raphydhar-Neezamme J. Ibrahim</h1>
 
                 <hr className="w-auto border-2 rounded-full border-[#30322d] my-4" />
 
-                <p className="text-3xl">I began programming during my senior high school year. Discovering that programming can be a form of art, hence I began to learn game development which branched my learning from level design and creating my own game assets.</p>
+                <p className="text-sm lg:text-3xl">I began programming during my senior high school year. Discovering that programming can be a form of art, hence I began to learn game development which branched my learning from level design and creating my own game assets.</p>
               </div>
 
-              <img src="capybara.jpg" alt="image" className="h-70 w-96 bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d]" />
+              <img src="capybara.jpg" alt="image" className="h-70 w-96 hidden lg:flex bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d]" />
             </div>
           </div>
         </div>
 
-        <div className='w-7xl h-content bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d] overflow-hidden shadow-lg'>
+        {/* Skills */}
+        <div className='w-70 lg:w-6xl bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d] overflow-hidden shadow-lg'>
           <div className='p-2.5 bg-[#f6d7d4] flex justify-between items-center'>
-            <p className='flex-grow text-center text-3xl mt-2'>www.Skillz.com</p>
+            <p className='flex-grow text-center text-1xl lg:text-3xl'>More About Me</p>
             <a className='w-8 rounded-[100%] bg-red-400 flex justify-center items-center border-4' href="#hero">‎</a>
           </div>
 
           <hr className='border-b-4' />
 
-          <div className='flex flex-col p-15 gap-25 items-center'>
-            <div className="grid-cols-3 grid gap-8">
-              <div className="border-4 border-[#30322d] rounded-2xl p-10 shadow-lg flex flex-col items-center h-130">
-                <img src="backend.png" alt="image" className="w-30 mb-10" />
+          <div className='flex flex-col p-5 lg:p-15 gap-5 lg:items-center lg:w-full'>
+            <div className="flex lg:hidden gap-2">
+              <button className="p-2 outline-2 rounded font-bold w-15" onClick={handlePrev}> { "<" } </button>
+              <button className="p-2 outline-2 rounded font-bold w-15" onClick={handleNext}> { ">" } </button>
+              <p className="p-2 ps-2 outline-2 rounded w-full bg-white">http://skills.com</p>
+            </div>
 
-                <h1 className="text-3xl text-center mb-5">Backend <br /> Development</h1>
-                <p className="text-2xl text-center">Logic can be difficult to implement! But with enough time I can tailor the functionalities according to your needs!</p>
+            <div className="grid-cols-3 grid gap-8 h-fit">
+              <div className={`border-4 border-[#30322d] rounded-2xl p-10 shadow-lg flex flex-col items-center lg:h-130 w-58 lg:w-fit ${index === 0 ? 'block' : 'hidden'} lg:flex`}>
+                <img src="backend.png" alt="image" className="w-15 lg:w-30 mb-10" />
+
+                <h1 className="text-2xl lg:text-3xl text-center mb-5 font-bold">Backend <br /> Development</h1>
+                <p className="text-sm lg:text-2xl text-center">Logic can be difficult to implement! But with enough time I can tailor the functionalities according to your needs!</p>
               </div>
 
-              <div className="border-4 border-[#30322d] rounded-2xl p-10 shadow-lg flex flex-col items-center h-130">
-                <img src="frontend.png" alt="image" className="w-30 mb-10" />
+              <div className={`border-4 border-[#30322d] rounded-2xl p-10 shadow-lg flex flex-col items-center lg:h-130 w-58 lg:w-fit ${index === 1 ? 'block' : 'hidden'} lg:flex`}>
+                <img src="frontend.png" alt="image" className="w-15 lg:w-30 mb-10" />
 
-                <h1 className="text-3xl text-center mb-5">Frontend <br /> Development</h1>
-                <p className="text-2xl text-center">A project can go on without design, but why settle for less! I enjoy designing themes and I integrate creative magic to the projects I tackle.</p>
+                <h1 className="text-2xl lg:text-3xl text-center mb-5 font-bold">Frontend <br /> Development</h1>
+                <p className="text-sm lg:text-2xl text-center">A project can go on without design, but it doesn't have to! I enjoy designing the projects I tackle.</p>
               </div>
 
-              <div className="border-4 border-[#30322d] rounded-2xl p-10 shadow-lg flex flex-col items-center h-130">
-                <img src="3d.png" alt="image" className="w-30 mb-10" />
+              <div className={`border-4 border-[#30322d] rounded-2xl p-10 shadow-lg flex flex-col items-center lg:h-130 w-58 lg:w-fit ${index === 2 ? 'block' : 'hidden'} lg:flex`}>
+                <img src="3d.png" alt="image" className="w-15 lg:w-30 mb-10" />
 
-                <h1 className="text-3xl text-center mb-5">∋D <br /> Modelling</h1>
-                <p className="text-2xl text-center">I create my own ∋D assets to make everything in a game or website feel that they belong on the same palette.</p>
+                <h1 className="text-2xl lg:text-3xl text-center mb-5 font-bold">3D <br /> Modelling</h1>
+                <p className="text-sm lg:text-2xl text-center">I create my own ∋D assets to make everything in a game or website feel that they belong on the same palette.</p>
               </div>
             </div>
           </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,42 +2,42 @@ export function Hero() {
     return (
         <div className='flex h-screen' id='hero'>
             {/* Navigation Bar */}
-            <div className='absolute flex flex-col items-start p-20 gap-20'>
-                <a className='w-25 outline-0 cursor-default text-center hover:bg-[#fbf5e7] rounded-lg p-2' href='#about-me'>
+            <div className='absolute flex lg:flex-col p-5 lg:p-20 gap-5 lg:gap-20 w-full items-center lg:items-start justify-center pt-15'>
+                <a className='w-15 lg:w-25 outline-0 cursor-default text-center hover:bg-[#fbf5e7] rounded-lg p-2' href='#about-me'>
                     <img src="old-computer.png" alt="image" />
-                    About Me
+                    About
                 </a>
 
-                <a className='w-25 outline-0 hover:bg-[#fbf5e7] rounded-lg p-2' href="#projects">
+                <a className='w-15 lg:w-25 outline-0 cursor-default text-center hover:bg-[#fbf5e7] rounded-lg p-2' href="#projects">
                     <img src="open-folder.png" alt="image" />
-                    <p className='font-medium'>My Projects</p>
+                    <p className='font-medium'>Projects</p>
                 </a>
 
-                <button className='w-25 outline-0 hover:bg-[#fbf5e7] rounded-lg p-2'>
+                <a className='w-15 lg:w-25 outline-0 cursor-default text-center hover:bg-[#fbf5e7] rounded-lg p-2' href="#">
                     <img src="globe-earth.png" alt="image" />
                     <p className='font-medium'>Explorer</p>
-                </button>
+                </a>
             </div>
 
             {/* Hero */}
             <div className='flex flex-col w-screen items-center justify-center overflow-hidden'>
-                <div className='w-4xl h-content bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d] overflow-hidden shadow-lg'>
+                <div className='w-70 lg:w-1/2 h-content bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d] overflow-hidden shadow-lg'>
                     <div className='p-2.5 bg-[#c3e7e5] flex justify-between items-center'>
-                        <p className='flex-grow text-center text-3xl mt-2'>The Raphysitory</p>
-                        <button className='w-8 rounded-[100%] bg-red-400 flex justify-center items-center border-4' href="#hero">‎</button>
+                        <p className='flex-grow text-center text-1xl lg:text-3xl'>The Raphysitory</p>
+                        <button className='w-8 rounded-[100%] bg-red-400 flex justify-center items-center border-4'>‎</button>
                     </div>
 
                     <hr className='border-b-4' />
 
-                    <div className='flex p-8 gap-8'>
-                        <img src="capybara.jpg" alt="image" className="h-70 w-96 bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d]" />
+                    <div className='flex flex-col lg:flex-row p-4 lg:p-8 gap-4 lg:gap-8 items-center'>
+                        <img src="capybara.jpg" alt="image" className="h-25 w-25 lg:h-70 lg:w-70 bg-[#fbf5e7] rounded-2xl border-4 border-[#30322d]" />
 
                         <div className="w-full">
-                            <div className="flex items-center mb-10">
-                                <p className="text-6xl">Hi, I'm <span className="bold">Raphy!</span></p>
+                            <div className="flex items-center mb-2 lg:mb-8">
+                                <p className="text-2xl lg:text-5xl text-center w-full font-bold">Hi, I'm <span className="bold">Raphy!</span></p>
                             </div>
 
-                            <p className="text-3xl mb-7">An aspiring programmer that focuses on full-stack development. Valuing both art and logic to give life into projects.</p>
+                            <p className="text-sm lg:text-3xl text-justify">An aspiring programmer that focuses on full-stack development. Valuing both art and logic to give life into projects.</p>
                         </div>
                     </div>
                 </div>
